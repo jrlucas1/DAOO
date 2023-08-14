69c657f197f97794a91eccdac184770fda1a61c9
João Raul F. Lucas <joaolucas.pl151@academico.ifsul.edu.br>
2023-08-14 19:24:37 -0300

    feat: condição isNormal adicionad

--- Atividade 2/Classes/traits/Imc.php
+++ Atividade 2/Classes/traits/Imc.php
@@ -40,6 +40,20 @@ trait IMC{
             echo "Obesidade\n";
         }
     }
+
+    public function isNormal(){
+        if (($this->idade>=19 && $this->idade<=24 && $this->imc>=19 && $this->imc <=24) || 
+            ($this->idade>24 && $this->idade<=34 && $this->imc>=20 && $this->imc <=25) ||
+            ($this->idade>34 && $this->idade<=44 && $this->imc>=21 && $this->imc <=26) ||
+            ($this->idade>44 && $this->idade<=54 && $this->imc>=22 && $this->imc <=27) ||
+            ($this->idade>54 && $this->idade<=64 && $this->imc>=23 && $this->imc <=28) ||
+            ($this->idade>64 && $this->imc>=24 && $this->imc <=29)
+            ){
+                return true;
+            } else {
+                echo "IMC inadequado.\n";
+            }
+        }
 }
 
 ?>
