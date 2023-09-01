f7cbd07b955f66fb0a0297ca1f23045dbecc7865
João Raul F. Lucas <joaolucas.pl151@academico.ifsul.edu.br>
2023-09-01 11:12:05 -0300

    feat: created Atividade model & controller

---
+++ Atividade 4/src/routes.php
@@ -0,0 +1,24 @@
+<?php
+
+use Daoo\Aula03\controller\Route;
+use Daoo\Aula03\controller\api\Produto;
+// use Daoo\Aula03\controller\api\Desconto;
+
+Route::routes([
+	'produto' => Produto::class,
+	//'desconto' => Desconto::class
+	// 'usuario' => Usuaraio::class
+]);
+
+//api:
+// composer run api
+// ou
+// php -S localhost:8081 src/index.php
+//
+//http://localhost:8081/classe/metodo/parametro
+//http://localhost:8081/produto/show/111
+
+// composer run web
+// ou
+// php -S localhost:8080 -t src/web/ 
+//http://localhost:8081/showProduto.php?id=111
